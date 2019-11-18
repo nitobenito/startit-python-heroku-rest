b1514b04b6f007b7790eab4dc6da67403c411209
nitobenito <52706184+nitobenito@users.noreply.github.com>
2019-11-18 14:58:30 +0200

    Update _init_.py

--- app/_init_.py
+++ app/_init_.py
@@ -1,1 +1,29 @@
+from flask import Flask, request, render_template
+import json
+# šī rinda laikam palaiž mūsu kodu kā flask api
+app = Flask(__name__)
+
+# Uzstada test.html ka nokluseto web lapas sākumu.
+@app.route('/')
+def root():
+    return render_template('test.html')
+
+@app.route('/api/rezultati')
+def rez():
+    vards=request.args.get("vards")
+    punkti=request.args.get("punkti")
+    f=open('vardi.txt','r')
+    saraksts=json.load(f)
+    f.close()
+    jauns={"vards":vards,"punkti":int(punkti)}
+    saraksts.append(jauns)
+    f=open('vardi.txt','w')
+    json.dump(saraksts,f)
+    f.close()
+    response = {
+        'rezultati': sorted(saraksts,key=lambda vi: vi["punkti"])
+    }
+    return json.dumps(response)
+
+app.run()
 
