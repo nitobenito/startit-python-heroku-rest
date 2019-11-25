24fca282854e7943761aee91b17dea5af76d75d7
nitobenito <52706184+nitobenito@users.noreply.github.com>
2019-11-25 16:27:33 +0200

    Update main.py

--- main.py
+++ main.py
@@ -25,6 +25,8 @@ def rez():
         'rezultati': sorted(saraksts,key=lambda vi: vi["punkti"])
     }
     return json.dumps(response)
+if __name__ == '__main__':
+    # Threaded option to enable multiple instances for multiple user access support
+    app.run(threaded=True, port=5000)
 
-app.run()
 
