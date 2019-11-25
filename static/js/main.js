f7eb1576b69acf36ec2a4083db5fe6aa5de900c8
nitobenito <52706184+nitobenito@users.noreply.github.com>
2019-11-25 09:29:55 +0200

    Update main.js

--- static/js/main.js
+++ static/js/main.js
@@ -1,10 +1,11 @@
- let laiks = 60; 
-  function pieskaitaPunktu() {
+window.onload=pieskaitaPunktu();
+let laiks = Math.floor(Math.random()*61); 
+function pieskaitaPunktu() {
     let vards=prompt("Ievadi savu vardu!");
-    //s="{\"vards\": \""+vards+"\",\"punkti\": "+laiks+"}";
-    s={"vards":vards,"punkti":laiks};
-    dabutRezultatuTabulu(s, paraditRezultatuTabulu);
+    let s={"vards":vards,"punkti":laiks};
+    dabutRezultatuTabulu(s, paraditRezultatuTabulu); 
   }
+
   function paraditRezultatuTabulu(rez){
     let saraksts=document.createElement("OL");
     for (var r in rez){
@@ -14,14 +15,14 @@
     }
     document.getElementById("spele").appendChild(saraksts);
   }
-  function dabutRezultatuTabulu(s, fnKadTabulaDabuta){
-    var urla = new URL("http://127.0.0.1:5000/api/rezultati");
-    Object.keys(s).forEach(key => urla.searchParams.append(key,s[key]))
+
+  function dabutRezultatuTabulu(o, fnKadTabulaDabuta){
+    var urla = new URL("https://lynxserver.herokuapp.com//api/rezultati");  
+    Object.keys(o).forEach(key => urla.searchParams.append(key,o[key]));
     fetch(new Request(urla))
       .then((resp)=>resp.json())
       .then((data)=>{
         console.log(data.rezultati);
         fnKadTabulaDabuta(data.rezultati);
-      });
+     });
   }
-
