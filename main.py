244d3f7e6f4e8893d3ca29d4a6e32813ebcf49c9
nitobenito <52706184+nitobenito@users.noreply.github.com>
2020-02-02 13:43:09 +0200

    Update main.py

--- main.py
+++ main.py
@@ -23,6 +23,15 @@ def rez():
         'rezultati': sorted(saraksts,key=lambda vi: vi["punkti"])
     }
     return json.dumps(response)
+@app.route('/api/rezultati', methods=['POST'])
+def suutiit_zinju():
+    vards=request.args.put("vards")
+    punkti=request.args.put("punkti")
+    response = {
+        'vards':'Feb03',
+        'punkti':'111'
+    }
+    return json.dumps(response)
 #dīvaini, bet iekš Heroku šis if bija izšķirošais lai sāktu strādāt
 if __name__ == '__main__':
     # Threaded option to enable multiple instances for multiple user access support
