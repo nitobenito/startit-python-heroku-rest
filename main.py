f9a4b75ddd60cebd907b267c6c68a309f10fd4c4
nitobenito <52706184+nitobenito@users.noreply.github.com>
2020-02-02 17:21:56 +0200

    Update main.py

--- main.py
+++ main.py
@@ -27,7 +27,18 @@ def atbilde():
     data = request.get_json()
     vards = data.get('vards', '')
     punkti  = data.get('punkti', '')
-    return vards+" "+punkti
+    f=open('app/vardi.txt','r')
+    saraksts=json.load(f)
+    f.close()
+    jauns={"vards":vards,"punkti":int(punkti)}
+    saraksts.append(jauns)
+    f=open('app/vardi.txt','w')
+    json.dump(saraksts,f)
+    f.close()
+    response = {
+        'rezultati': sorted(saraksts,key=lambda vi: vi["punkti"])
+    }
+    return json.dumps(response)
 #dīvaini, bet iekš Heroku šis if bija izšķirošais lai sāktu strādāt
 if __name__ == '__main__':
     # Threaded option to enable multiple instances for multiple user access support
