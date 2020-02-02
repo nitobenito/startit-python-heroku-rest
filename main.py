794fec678021610c7a0f115cf8986e147c95e52e
nitobenito <52706184+nitobenito@users.noreply.github.com>
2020-02-02 16:12:25 +0200

    Update main.py

--- main.py
+++ main.py
@@ -25,10 +25,7 @@ def rez():
     return json.dumps(response)
 @app.route('/api/rezultati', methods=['POST'])
 def atbilde():
-    dati = request.json
-    vards=dati["vards"]+"01"
-    punkti=dati["punkti"]
-    return vards+" "+punkti
+    return "atbild"
 #dīvaini, bet iekš Heroku šis if bija izšķirošais lai sāktu strādāt
 if __name__ == '__main__':
     # Threaded option to enable multiple instances for multiple user access support
