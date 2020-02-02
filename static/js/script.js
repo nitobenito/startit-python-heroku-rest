136a3c50c5881c20d6c013ff9561c3f9874fd613
nitobenito <52706184+nitobenito@users.noreply.github.com>
2020-02-02 18:24:34 +0200

    Update script.js

--- static/js/script.js
+++ static/js/script.js
@@ -28,31 +28,3 @@ function pieskaitaPunktu() {
     }
     document.getElementById("spele").appendChild(saraksts);
   }
-
-  function dabutRezultatuTabulu1(o, fnKadTabulaDabuta){
-    var urla = new URL("https://alynxserver.herokuapp.com/api/rezultati");  
-    Object.keys(o).forEach(key => urla.searchParams.append(key,o[key]));
-    fetch(new Request(urla))
-      .then((resp)=>resp.json())
-      .then((data)=>{
-        console.log(data.rezultati);
-        fnKadTabulaDabuta(data.rezultati);
-     });
-  }
- function dabutRezultatuTabulu(o, fnKadTabulaDabuta){
-   console.log(JSON.stringify(o));
-   const url = 'https://alynxserver.herokuapp.com/api/rezultati';
-   var request = new Request(url, {
-   method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(o)
-});
-    fetch(request)
-      .then((resp)=>resp.json())
-      .then((data)=>{
-        console.log(data.rezultati);
-        fnKadTabulaDabuta(data.rezultati);
-     });
-  }
